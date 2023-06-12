f44d9b7d0509a1218f35d3bb41671435fb8d4662
Eoghain O'Briain <eoghain.obriain@appliedtechnology.se>
2023-06-12 11:07:57 +0200

    refresh and loading updates

--- src/pages/Results.tsx
+++ src/pages/Results.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { AppContext } from "../AppContext";
 import { ContextType } from "../types";
